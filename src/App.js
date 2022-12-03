663936cafc73d0f68318825595c4e49a6b5a8786
Hrishikesh Patil <hrishi0102business@gmail.com>
2022-12-04 02:50:07 +0530

    SBT v1

---
+++ src/App.js
@@ -0,0 +1,172 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { ethers } from "ethers";
+import sbtContract from "./ethereum/sbt";
+
+function App() {
+  const [walletAddress, setWalletAddress] = useState("");
+  const [signer, setSigner] = useState();
+  const [SbtContract, setSbtContract] = useState();
+  const [SendSuccess, setSendSuccess] = useState("");
+  const [SendError, setSendError] = useState("");
+  const [TransactionData, setTransactionData] = useState();
+
+  useEffect(() => {
+    getCurrentWalletConnected();
+    addWalletListener();
+  }, [walletAddress]);
+
+  const connectWallet = async () => {
+    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+      try {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const accounts = await window.ethereum.request(
+          "eth_requestAccounts",
+          []
+        );
+        setSigner(provider.getSigner());
+        setSbtContract(sbtContract(provider));
+
+        setWalletAddress(accounts[0]);
+        console.log(accounts[0]);
+      } catch (err) {
+        console.error(err.message);
+      }
+    } else {
+      /* MetaMask is not installed */
+      console.log("Please install MetaMask");
+    }
+  };
+
+  const getCurrentWalletConnected = async () => {
+    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+      try {
+        /* get provider */
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        /* get accounts */
+        const accounts = await provider.send("eth_accounts", []);
+        if (accounts.length > 0) {
+          /* get signer */
+          setSigner(provider.getSigner());
+          /* local contract instance */
+          setSbtContract(sbtContract(provider));
+          /* set active wallet address */
+          setWalletAddress(accounts[0]);
+        } else {
+          console.log("Connect to MetaMask using the Connect Wallet button");
+        }
+      } catch (err) {
+        console.error(err.message);
+      }
+    } else {
+      /* MetaMask is not installed */
+      console.log("Please install MetaMask");
+    }
+  };
+
+  const addWalletListener = async () => {
+    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        setWalletAddress(accounts[0]);
+        console.log(accounts[0]);
+      });
+    } else {
+      /* MetaMask is not installed */
+      setWalletAddress("");
+      console.log("Please install MetaMask");
+    }
+  };
+
+  const sendCBT = async () => {
+    setSendError("");
+    setSendSuccess("");
+    try {
+      const sbtwithSigner = SbtContract.connect(signer);
+      const resp = await sbtwithSigner.safeMint(
+        document.getElementById("mintadd").value,
+        "QmRGEzZ8heZ4pXfshrZv6njzbmQxWU6t9d1fspvD6R7j7B"
+      );
+      console.log(resp);
+      setSendSuccess("Successful");
+      setTransactionData(resp.hash);
+    } catch (err) {
+      console.error(err.message);
+      setSendError(err.message);
+    }
+  };
+
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="container">
+          <div className="navbar-brand">
+            <h1 className="navbar-item is-size-4">CrimeBound</h1>
+          </div>
+          <div id="navbarMenu" className="navbar-menu">
+            <div className="navbar-end is-align-items-center">
+              <button
+                className="button is-white connect-wallet"
+                onClick={connectWallet}
+              >
+                <span className="is-link has-text-weight-bold">
+                  {walletAddress && walletAddress.length > 0
+                    ? `Connected: ${walletAddress.substring(
+                        0,
+                        6
+                      )}...${walletAddress.substring(38)}`
+                    : "Connect Wallet"}
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <section className="hero is-fullheight">
+        <div className="faucet-hero-body">
+          <div className="container has-text-centered main-content">
+            {/* <h2 className="title is-1"></h2> */}
+            <div className="mt-5">
+              {SendError && <div className="withdraw-error">{SendError}</div>}
+              {SendSuccess && (
+                <div className="withdraw-success">{SendSuccess}</div>
+              )}{" "}
+            </div>
+            <div className="box address-box">
+              <div className="columns">
+                <div className="column is-four-fifths">
+                  <input
+                    className="input is-medium"
+                    type="text"
+                    id="mintadd"
+                    placeholder="Enter wallet address (0x...)"
+                  />
+                </div>
+                <div className="column">
+                  <button
+                    className="button is-link is-medium"
+                    onClick={sendCBT}
+                    disabled={walletAddress ? false : true}
+                  >
+                    Send CBT
+                  </button>
+                </div>
+              </div>
+              <article className="panel is-grey-darker">
+                <p className="panel-heading">Transaction Data</p>
+                <div className="panel-block">
+                  <p>
+                    {TransactionData
+                      ? `Transaction hash: ${TransactionData}`
+                      : " "}
+                  </p>
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
